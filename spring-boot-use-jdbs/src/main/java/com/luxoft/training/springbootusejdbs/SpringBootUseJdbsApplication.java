42d9255e690d79623fba4e28927f387ffbaa7b5e
Ruslan Abdrakhmanov <r.abdrakhmanov@haulmont.com>
2021-03-31 19:17:31 +0400

    init commit

---
+++ spring-boot-use-jdbs/src/main/java/com/luxoft/training/springbootusejdbs/SpringBootUseJdbsApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft.training.springbootusejdbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUseJdbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUseJdbsApplication.class, args);
+	}
+
+}
